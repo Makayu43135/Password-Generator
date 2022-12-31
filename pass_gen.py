4b765754d4849454c399c5d5fcdb7e75da0a653f
Makayu43135 <96719643+Makayu43135@users.noreply.github.com>
2022-12-31 13:17:21 +0700

    Add files via upload

---
+++ pass_gen.py
@@ -0,0 +1,11 @@
+import secrets
+import string
+
+password_length = int(input("Please enter the length of the password: "))
+
+alphabet = string.ascii_letters + string.digits
+symbols = "`~!@#$%^&*()_+|\}{:?><,./;'[]"
+password = ''.join(secrets.choice(alphabet + symbols) for i in range(password_length))
+print("Generated Password: " + password)
+
+x = input("Done, press Enter to exit")
