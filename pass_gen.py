3ad957ad7fea1cda2d866c29d30b514bc1ea1d97
Makayu43135 <96719643+Makayu43135@users.noreply.github.com>
2023-01-02 17:28:05 +0700

    Update pass_gen.py

--- pass_gen.py
+++ pass_gen.py
@@ -1,12 +1,18 @@
 import secrets
 import string
 
-password_length = int(input("Please enter the length of the password: "))
+password_length = int(input("Please enter the length of the password(CANNOT BE HIGHER THAN 9999999): "))
 
 alphabet = string.ascii_letters + string.digits
 symbols = "`~!@#$%^&*()_+|\}{:?><,./;'[]"
 password = ''.join(secrets.choice(alphabet + symbols) for i in range(password_length))
-print("Generated Password: " + password)
+#password max cap
+if password_length >= 10000000:
+    print("Password length cannot be higher than 9999999!")
+    int(input("Please enter the length of the password(CANNOT BE HIGHER THAN 9999999): "))
+    print(password)
+else:
+    print(password)
 
+#enter to exit (OMG!!!)
 x = input("Done, press Enter to exit")
-# simple code isnt it?
